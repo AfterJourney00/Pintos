7203bec85d8196b57a7eb196a16037fd373cc6a5
yaoyq@VM <yyqjshcn@qq.com>
2020-10-16 13:07:16 +0000

    project 2 init

--- src-proj2/userprog/process.c
+++ src-proj2/userprog/process.c
@@ -19,7 +19,7 @@
 #include "threads/vaddr.h"
 
 static thread_func start_process NO_RETURN;
-static bool load (const char *cmdline, void (**eip) (void), void **esp);
+static bool load (const char *cmdline[], void (**eip) (void), void **esp, int argc);
 
 /* Starts a new thread running a user program loaded from
    FILENAME.  The new thread may be scheduled (and may even exit)
@@ -30,6 +30,7 @@ process_execute (const char *file_name)
 {
   char *fn_copy;
   tid_t tid;
+  char *exec_name = NULL, *save_ptr = NULL;    /* For parsing. */
 
   /* Make a copy of FILE_NAME.
      Otherwise there's a race between the caller and load(). */
@@ -38,8 +39,14 @@ process_execute (const char *file_name)
     return TID_ERROR;
   strlcpy (fn_copy, file_name, PGSIZE);
 
+  /* Parse file_name to have exec_name. */
+  exec_name = strtok_r(file_name, " ", &save_ptr);
+
+  if (exec_name == NULL)
+    return -1;
+  
   /* Create a new thread to execute FILE_NAME. */
-  tid = thread_create (file_name, PRI_DEFAULT, start_process, fn_copy);
+  tid = thread_create (exec_name, PRI_DEFAULT, start_process, fn_copy);
   if (tid == TID_ERROR)
     palloc_free_page (fn_copy); 
   return tid;
@@ -53,13 +60,24 @@ start_process (void *file_name_)
   char *file_name = file_name_;
   struct intr_frame if_;
   bool success;
+  char *save_ptr;
+  char *argv[25];                 /* The splitted command line here. */
+  int argc = 0;                   /* Count of arguments passed in on one command line. */
+
+  for (char *token = strtok_r ((char *)file_name, " ", &save_ptr);
+       token != NULL;
+       token = strtok_r (NULL, " ", &save_ptr))
+  {
+    argv[argc] = token;
+     argc++;
+  }
 
   /* Initialize interrupt frame and load executable. */
   memset (&if_, 0, sizeof if_);
   if_.gs = if_.fs = if_.es = if_.ds = if_.ss = SEL_UDSEG;
   if_.cs = SEL_UCSEG;
   if_.eflags = FLAG_IF | FLAG_MBS;
-  success = load (file_name, &if_.eip, &if_.esp);
+  success = load (file_name, &if_.eip, &if_.esp, argc);
 
   /* If load failed, quit. */
   palloc_free_page (file_name);
@@ -195,7 +213,7 @@ struct Elf32_Phdr
 #define PF_W 2          /* Writable. */
 #define PF_R 4          /* Readable. */
 
-static bool setup_stack (void **esp);
+static bool setup_stack (void **esp, char *argv[], int argc);
 static bool validate_segment (const struct Elf32_Phdr *, struct file *);
 static bool load_segment (struct file *file, off_t ofs, uint8_t *upage,
                           uint32_t read_bytes, uint32_t zero_bytes,
@@ -206,7 +224,7 @@ static bool load_segment (struct file *file, off_t ofs, uint8_t *upage,
    and its initial stack pointer into *ESP.
    Returns true if successful, false otherwise. */
 bool
-load (const char *file_name, void (**eip) (void), void **esp) 
+load (const char *file_name[], void (**eip) (void), void **esp, int argc) 
 {
   struct thread *t = thread_current ();
   struct Elf32_Ehdr ehdr;
@@ -222,7 +240,7 @@ load (const char *file_name, void (**eip) (void), void **esp)
   process_activate ();
 
   /* Open executable file. */
-  file = filesys_open (file_name);
+  file = filesys_open (file_name[0]);
   if (file == NULL) 
     {
       printf ("load: %s: open failed\n", file_name);
@@ -302,7 +320,7 @@ load (const char *file_name, void (**eip) (void), void **esp)
     }
 
   /* Set up stack. */
-  if (!setup_stack (esp))
+  if (!setup_stack (esp, file_name, argc))
     goto done;
 
   /* Start address. */
@@ -427,7 +445,7 @@ load_segment (struct file *file, off_t ofs, uint8_t *upage,
 /* Create a minimal stack by mapping a zeroed page at the top of
    user virtual memory. */
 static bool
-setup_stack (void **esp) 
+setup_stack (void **esp, char *argv[], int argc) 
 {
   uint8_t *kpage;
   bool success = false;
@@ -437,7 +455,39 @@ setup_stack (void **esp)
     {
       success = install_page (((uint8_t *) PHYS_BASE) - PGSIZE, kpage, true);
       if (success)
-        *esp = PHYS_BASE;
+      {
+        *esp = PHYS_BASE;        /* Offset of the PHYS_BASE. */
+
+        uint32_t *argv_ptr[argc];     /* A list of address in the stack. */
+        
+        for (int i = argc - 1; i >= 0; i--)
+        {
+          /* Allocate space for the command line. */
+          *esp = *esp - sizeof(char) * (strlen (argv[i]) + 1);
+          memcpy (*esp, argv[i], sizeof(char) * (strlen (argv[i]) + 1));
+          argv_ptr[i] = (uint32_t*)*esp;
+        }
+
+        *esp = (uint32_t)*esp & 0xfffffffc;
+
+        *esp = *esp - 4;
+        (*(int *)(*esp)) = 0;
+
+        *esp = *esp - 4;
+        for (int i = argc - 1; i >= 0; i--)
+        {
+          (*(uint32_t **)(*esp)) = argv_ptr[i];
+          *esp = *esp - 4;
+        }
+
+        (*(uintptr_t **)(*esp)) = *esp + 4;
+
+        *esp = *esp - 4;
+        *(int *)(*esp) = argc;
+
+        *esp = *esp - 4;
+        (*(int *)(*esp)) = 0;
+      }
       else
         palloc_free_page (kpage);
     }
