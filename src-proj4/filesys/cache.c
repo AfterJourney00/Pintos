3776be3a40d30f4088e7dbcbe6c1e2228715a227
zhaochf <zhaochf@shanghaitech.edu.cn>
2020-12-05 16:20:41 +0800

    update buffer-cache

--- src-proj4/filesys/cache.c
+++ src-proj4/filesys/cache.c
@@ -37,13 +37,19 @@ cache_clear(void)
 }
 
 /* Function for checking cache hit or miss */
+/* This function will be called before every access to cache,
+   so update cache line's accessed_time here */
 struct cache_line*
 check_hit_or_not(block_sector_t sec)
 {
+  ASSERT(lock_held_by_current_thread(&cache_lock));
+
   struct cache_line* target_line = NULL;
   for(int i = 0; i < CACHE_SIZE; i ++){
     if(cache[i].valid_bit && sec == cache[i].sector_idx){
-      target_line = &cache[i];
+      target_line = &cache[i];                  /* Find the candidate line */
+      cache[i].accessed_time = timer_ticks();   /* Update its accessed time */
+      ASSERT(cache[i].available == false);      /* Assert this cache line is busy now */
       goto done;
     }
   }
@@ -60,10 +66,15 @@ cache_line_init(struct cache_line* cl)
 
   cl->valid_bit = true;
   cl->dirty_bit = false;
+  cl->available = true;
   cl->accessed_time = timer_ticks();
+  cl->buffer = (char*)malloc(BLOCK_SECTOR_SIZE);
+  ASSERT(cl->buffer != NULL);
+
+  return;
 }
 
-/* Clear a single cache line */
+/* Clear a single cache line (don't know yet)*/
 void
 cache_line_clear(struct cache_line* cl)
 {
@@ -71,6 +82,24 @@ cache_line_clear(struct cache_line* cl)
 
   cl->valid_bit = false;            /* Set this cache line as an invalid one */
   cl->dirty_bit = false;
+}
+
+/* Find a free cache line if cache miss */
+struct cache_line*
+fetch_a_free_cache_line(void)
+{
+  ASSERT(lock_held_by_current_thread(&cache_lock));
+
+  struct cache_line* target_line = NULL;
+  for(int i = 0; i < CACHE_SIZE; i ++){
+    if(cache[i].valid_bit && cache[i].available){
+      target_line = &cache[i];
+      goto done;
+    }
+  }
+
+done:
+  return target_line;
 }
 
 /* Function for choosing a victim cache line to evict, using LRU policy */
@@ -83,16 +112,95 @@ next_cache_line_to_evict(void)
   struct cache_line* target_line = NULL;
 
   for(int i = 0; i < CACHE_SIZE; i ++){
-    if(!cache[i].valid_bit){
+    ASSERT(cache[i].available == false);
+
+    if(!cache[i].valid_bit){    /* If the cache line is invalid, pass it whatever */
       continue;
     }
-    else{
-      if(cache[i].created_time < earliest_time){
-        earliest_time = cache[i].created_time;
+    else{                       /* Else, choose a victim line using LRU policy */
+      if(cache[i].accessed_time < earliest_time){
+        earliest_time = cache[i].accessed_time;
         target_line = &cache[i];
       }
     }
   }
 
   return target_line;
+}
+
+/* Function for cache line eviction */
+void
+evict_cache_line(struct cache_line* cl)
+{
+  ASSERT(cl != NULL);
+  ASSERT(cl->valid_bit == true && cl->available == false);
+  
+  if(cl->dirty_bit){
+    cache_write_back(cl);
+  }
+  /* cl->available = true; */
+  cl->dirty_bit = false;
+  return;
+}
+
+/* Write back from cache to disk */
+void
+cache_write_back(struct cache_line* cl)
+{
+  /* Assert the given cache line is a valid one */
+  ASSERT(cl != NULL);
+  ASSERT(cl->valid_bit == true && cl->dirty_bit == true);
+
+  block_write(fs_device, cl->sector_idx, cl->buffer);
+  return;
+}
+
+/* Fetch in from disk to cache */
+void
+cache_fetch_in(struct cache_line* cl)
+{
+  /* Assert the given cache line is a valid one */
+  ASSERT(cl != NULL);
+  ASSERT(cl->valid_bit == true);
+
+  block_read(fs_device, cl->sector_idx, cl->buffer);
+}
+
+/* Other parts through this function to access cache and do operations */
+/* read_or_write = true: read 
+   read_or_write = false: write */
+void
+cache_do(bool read_or_write, block_sector_t sec, void* mem_addr)
+{
+  lock_acquire(&cache_lock);
+
+  struct cache_line* target_line = check_hit_or_not(sec);
+  if(target_line == NULL){/* Cache miss */
+    target_line = fetch_a_free_cache_line();
+    if(target_line == NULL){          /* Need to do eviction */
+      target_line = next_cache_line_to_evict();
+      evict_cache_line(target_line);
+    }
+
+    ASSERT(target_line->valid_bit == true);
+
+    target_line->available = false;             /* Set this cache line as a busy line */
+    target_line->accessed_time = timer_ticks(); /* Update its accessed time */
+    target_line->sector_idx = sec;              /* Record the sector index */
+    target_line->dirty_bit = !read_or_write;
+    cache_fetch_in(target_line);
+  }
+
+  ASSERT(target_line != NULL);
+
+  if(read_or_write){
+    memcpy(mem_addr, (const void*)(target_line->buffer), BLOCK_SECTOR_SIZE);
+  }
+  else{
+    target_line->dirty_bit = true;
+    memcpy((void*)(target_line->buffer), (const void*)mem_addr, BLOCK_SECTOR_SIZE);
+  }
+
+  lock_release(&cache_lock);
+  return;
 }
--- src-proj4/filesys/cache.h
+++ src-proj4/filesys/cache.h
@@ -11,9 +11,11 @@ struct cache_line
 {
   bool valid_bit;                   /* Only use cache line with true valid bit */
   bool dirty_bit;                   /* Only write back to disk with true dirty bit */
+  bool available;                   /* Indicate whether this cache line is available */
   int64_t accessed_time;            /* Use LRU cache eviction policy */
 
-  block_sector_t sector_idx;       /* Record which sector should this cache line write back */
+  block_sector_t sector_idx;        /* Record which sector should this cache line write back */
+  char* buffer;                     /* Content of this cache line(512 bytes) */
 };
 
 /* The whole cache, an array of cache lines */
@@ -30,8 +32,11 @@ struct cache_line* check_hit_or_not(block_sector_t sec);
 /* Cache line operations */
 void cache_line_init(struct cache_line* cl);
 void cache_line_clear(struct cache_line* cl);
+struct cache_line* fetch_a_free_cache_line(void);
 struct cache_line* next_cache_line_to_evict(void);
-void cache_write_back(void);
-void cache_fetch_in(void);
+void evict_cache_line(struct cache_line* cl);
+void cache_write_back(struct cache_line* cl);
+void cache_fetch_in(struct cache_line* cl);
+void cache_do(bool read_or_write, block_sector_t sec, void* mem_addr);
 
 #endif
